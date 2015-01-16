e9c85feba264d97e12ae6f195432d48ce90a76ec
PERL <58036050@qq.com>
2015-01-16 15:43:15 +0800

    zz

---
+++ mid/localtime.pl
@@ -0,0 +1,10 @@
+#! /usr/bin/perl
+#
+#
+
+my @time = localtime;
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+#print "Time is : @time";
+
+print "sec :$sec,min :$min,hour :$hour,mday :$mday,mon :$mon,year $year,wday :$wday,yday :$yday,isdst :$isdst";
+
